3d33c7a36b4718590a32df71d9d1c82b74b2e6c4
myracledesign <weber.max.1988@live.de>
2020-01-30 19:51:45 +0100

    finished the future builder

---
+++ lib/model/person.dart
@@ -0,0 +1,13 @@
+class Person {
+  String name;
+  String phoneNumber;
+  String imageUrl;
+
+  Person({this.name, this.phoneNumber, this.imageUrl});
+
+  Person.fromJson(Map<String, dynamic> json)
+      : name =
+            "${json["name"]["title"]} ${json["name"]["first"]} ${json["name"]["last"]}",
+        phoneNumber = json["phone"],
+        imageUrl = json["picture"]["thumbnail"];
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:network_request/model/person.dart';
+import 'package:network_request/services/person_network_service.dart';
 
 class HomeScreen extends StatelessWidget {
+  final PersonNetworkService personService = PersonNetworkService();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: NetworkImage(
+              "https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80"),
+          fit: BoxFit.cover,
+        )),
+        child: SafeArea(
+          child: FutureBuilder(
+            future: personService.fetchPersons(100),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Person>> snapshot) {
+              if (snapshot.hasData) {
+                return Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        child: Card(
+                          color: Colors.black.withOpacity(0.5),
+                          child: ListView.builder(
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                var currentPerson = snapshot.data[index];
+
+                                return ListTile(
+                                  title: Text(currentPerson.name),
+                                  leading: CircleAvatar(
+                                    backgroundImage:
+                                        NetworkImage(currentPerson.imageUrl),
+                                  ),
+                                  subtitle: Text(
+                                      "Phone: ${currentPerson.phoneNumber}"),
+                                );
+                              }),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              }
+
+              if (snapshot.hasError) {
+                return Center(
+                    child: Icon(
+                  Icons.error,
+                  color: Colors.red,
+                  size: 82.0,
+                ));
+              }
+
+              return Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircularProgressIndicator(),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text("Loading at the moment, please hold the line.")
+                ],
+              ));
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
